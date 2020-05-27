b0198cd11cc6fdef5a3889303285723448d42cc0
AchyutGopal <40428597+AchyutGopal@users.noreply.github.com>
2020-05-28 04:55:24 +0530

    React Build

---
+++ build/precache-manifest.5afa6f48866a425883153b42bf64631f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46b923249ab7860e4911fd538c1ba07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4128ea4f3c771f1ebb7",
+    "url": "/static/css/main.aa89ed52.chunk.css"
+  },
+  {
+    "revision": "2704a3b53f52a5157a9b",
+    "url": "/static/js/2.31d0ea32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31d0ea32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4128ea4f3c771f1ebb7",
+    "url": "/static/js/main.613d4bf6.chunk.js"
+  },
+  {
+    "revision": "20d5c3d191ecb5beaab9",
+    "url": "/static/js/runtime-main.168993b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
